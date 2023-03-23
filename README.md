7a96333d39fedf1668dc0d336315d3e9ef1550a3
cbot918 <cbot918@gmail.com>
2023-03-23 14:39:10 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# gh-aws-ci
+
+quick test for integration github aws ci
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "pong\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+const (
+	port = ":8000"
+)
+
+func main() {
+
+	http.HandleFunc("/ping", hello)
+	http.HandleFunc("/headers", headers)
+
+	log := fmt.Println
+	log("listening:", port)
+
+	http.ListenAndServe(port, nil)
+}
